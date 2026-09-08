c6a4f9a0f77a519dfd5d3f49c5110341654ea63f
Dzy <3352717+Dzy@users.noreply.github.com>
2026-09-08 16:26:26 +0300

    Add TNC155 firmware application interface

---
+++ Core/Inc/tnc155_firmware.h
@@ -0,0 +1,16 @@
+#ifndef TNC155_FIRMWARE_H
+#define TNC155_FIRMWARE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool TNC155_Firmware_Init(void);
+void TNC155_Firmware_Task(void);
+void TNC155_Firmware_LTDCReloadComplete(void);
+
+extern volatile uint32_t g_tnc155_last_slice_core_cycles;
+extern volatile uint32_t g_tnc155_max_slice_core_cycles;
+extern volatile uint32_t g_tnc155_last_frame_core_cycles;
+extern volatile uint8_t g_tnc155_faulted;
+
+#endif /* TNC155_FIRMWARE_H */
