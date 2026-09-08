d3b396feff79c7994a8223d13836deea83e9e76b
Dzy <3352717+Dzy@users.noreply.github.com>
2026-09-08 18:17:15 +0300

    Embed supplied TNC155 User RAM image

---
+++ Core/Inc/tnc155_user_ram_image.h
@@ -0,0 +1,10 @@
+#ifndef TNC155_USER_RAM_IMAGE_H
+#define TNC155_USER_RAM_IMAGE_H
+
+#include <stdint.h>
+
+#define TNC155_USER_RAM_IMAGE_SIZE 0x12000u
+
+extern const uint8_t g_tnc155_user_ram_image[TNC155_USER_RAM_IMAGE_SIZE];
+
+#endif
